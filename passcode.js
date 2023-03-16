f28b388f71514f07e5994fb1e86a778b9a9075ad
TateTheBait <123601564+TateTheBait@users.noreply.github.com>
2023-03-16 18:57:48 -0500

    PASSWORD CHANGES

--- passcode.js
+++ passcode.js
@@ -1,5 +1,6 @@
-b = prompt("What is the passcode?")
-if (b == "Potato")[
+var b = prompt("What is the passcode?")
+
+if (b = "Potato")[
     print("Game Links: "),
     print("Reactor knockoff: www.mathemapp.com")
 ]
