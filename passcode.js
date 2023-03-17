b10931e78d6ee1fc78e389e6c2dabd2bb933feaa
TateTheBait <123601564+TateTheBait@users.noreply.github.com>
2023-03-16 20:14:10 -0500

    STUFF

--- passcode.js
+++ passcode.js
@@ -1,6 +1,7 @@
 var b = prompt("What is the passcode?")
 
-if (b = "Potato")[
-    print("Game Links: "),
-    print("Reactor knockoff: www.mathemapp.com")
-]
+if (b == "Potato"){
+    document.write("Game Links:    "),
+
+    document.write("|    Reactor knockoff: www.mathemapp.com    |")} else {
+    document.write("If you got the password wrong you aren't supposed to be here")}
